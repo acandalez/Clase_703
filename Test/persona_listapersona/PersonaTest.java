96bd0324a7b818f318c3d7a7a955581344e382f4
acandalez <acandalez@gmail.com>
2015-05-29 16:23:22 +0200

    Carpeta Test

---
+++ Test/persona_listapersona/PersonaTest.java
@@ -0,0 +1,26 @@
+package persona_listapersona;
+
+import static org.junit.Assert.assertEquals;
+import java_se.ejercicio_listapersona.Persona;
+
+import org.junit.Test;
+
+public class PersonaTest {
+
+	static Persona testpersona;
+
+	public static void Persona() {
+
+		testpersona = new Persona("", 0);
+
+	}
+
+	@Test
+	public void testEquals() {
+		// fail("Not yet implemented");
+		Persona p = new Persona("Juan", 21);
+		Persona p1 = new Persona("Juan", 21);
+		assertEquals(p.equals(testpersona), p1.equals(testpersona));
+	}
+
+}
