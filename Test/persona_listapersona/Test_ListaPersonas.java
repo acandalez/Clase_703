490afb5a5adf5c7bf566fa9f6beccb0851e86c2f
Leofix <acandalez@gmail.com>
2015-06-01 13:24:54 +0200

    01/06/2015

--- Test/persona_listapersona/Test_ListaPersonas.java
+++ Test/persona_listapersona/Test_ListaPersonas.java
@@ -1,29 +1,48 @@
 package persona_listapersona;
 
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
+import java_se.ejercicio_listapersona.InsertarPersonaException;
 import java_se.ejercicio_listapersona.ListaPersonas;
 import java_se.ejercicio_listapersona.Persona;
 
 import org.junit.Test;
 
 public class Test_ListaPersonas {
+	
+	public static ListaPersonas lp;
+	public static Persona p1 = new Persona("Pedro",30);
+	public static Persona p2 = new Persona("Luis",28);
+	public static Persona p3 = new Persona("Juan",40);
 
 	@Test
 	public void testBuscarPersona() {
 		// fail("Not yet implemented");
+		assertNotNull(lp);
 	}
 
-	public void Listapersonas_array() {
+	public void Listapersonas() {
 
 		ListaPersonas lp = new ListaPersonas();
-		Persona[] array = lp.getListaPersonas();
-
-		for (Persona p : array) {
-			assertNull(p);// comprobamos que en array cada posicion de null.
+		lp = new ListaPersonas();
+		try {
+			lp.insertarPersona(p1);
+			lp.insertarPersona(p2);
+			lp.insertarPersona(p3);
+		} catch (InsertarPersonaException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 	}
+	@Test
+	public void testBuscarPersonaNombre() {//buscar por nombre
+		assertEquals("Test buscar p1",p1,lp.buscarPersona("Pedro"));
+	}
 
+	@Test
+	public void testBuscarPersonaEdad() {
+		assertEquals("Test buscar p1",p1,lp.busarPersona(30));
+	}
 	// Ejemplos con Matchers
 
 }
--- src/java_se/XML_JAVA/practica2_JDOM/JDOM2.java
+++ src/java_se/XML_JAVA/practica2_JDOM/JDOM2.java
@@ -6,8 +6,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
@@ -16,9 +18,11 @@ import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
 
 /**
- * 1. parsear xml. 2. Crear array_list. 3. Ordeno Array. 4. Genero nuevo
- * Document. 5. Escribo fichero.
- * 
+ * 1. Parseo libros.xml (JDOM)
+ * 2. Hago el ArrayList>libro>
+ * 3. Ordeno ArrayList(por título)
+ * 4. Creo el nuevo documentJDOM(consultar API JDOM)
+ * 5. Serializo un nuevo XML
  * 
  * @author acandalez
  *
@@ -28,87 +32,92 @@ public class JDOM2 {
 	public static void main(String[] args) throws FileNotFoundException,
 			JDOMException, IOException {
 
-		Libro lb = new Libro();
-		int c = 0;
-		ArrayList<Libro> aLb = new ArrayList<Libro>();
-
-		// CREAMOS EL PARSER
-		SAXBuilder builder = new SAXBuilder();
-		// Construimos el arbol DOM a partir del fichero xml
-		Document documentJDOM = builder
-				.build(new FileInputStream("libros.xml"));
-
-		// MOSTRAMOS EL DOCUMENTO
-		Element raiz = documentJDOM.getRootElement();
-		// Recorremos los hijos de la etiqueta raíz
-		List<Element> libros = raiz.getChildren();
-		for (Element libro : libros) {
-
-			libros = aLb.get(c);
-			// para cada libro, obtenemos su detalle
-			// System.out.println(libro.getAttribute("isbn"));
-			// List<Element> detalles = libro.getChildren();
-
-			for (Element detalle : detalles) {
-
-				switch (detalle.getName()) {
-				case "titulo":
-					lb.setTitulo(detalle.getValue());
-
-					break;
-
-				case "autor":
-					lb.setTitulo(detalle.getValue());
-
-					break;
-				case "anyo":
-					lb.setTitulo(detalle.getValue());
-
-					break;
-				case "editorial":
-					lb.setTitulo(detalle.getValue());
-
-					break;
-
+		// 1.Parseo libros.xml (JDOM
+				SAXBuilder builder = new SAXBuilder();
+				// Construimos el arbol DOM a partir del fichero xml
+				Document documentJDOM = builder.build(new FileInputStream("libros.xml")); //La clase documentJDOM es un XML en memoria
+		// 2. Hago el ArrayList>libro>
+				ArrayList<Libro> listalibros = new ArrayList<Libro>();
+				
+				Element padre = documentJDOM.getRootElement();
+				
+				List<Element> libros = padre.getChildren();
+				
+				for (Element libro : libros) {
+					
+					Libro book = new Libro();
+					String isbn = libro.getAttribute("isbn").getValue();
+					book.setIsbn(isbn);
+					
+					List<Element> detalles = libro.getChildren();
+					
+					for (Element detalle : detalles) {
+						
+						switch (detalle.getName()) {
+						
+						case "titulo": book.setTitulo(detalle.getValue());
+								break;
+						case "autor" : book.setAutor(detalle.getValue());
+								break;
+						case "anyo"  : book.setAnyo(detalle.getValue());
+								break;
+						case "editorial": book.setEditorial(detalle.getValue());
+								break;
+							
+						}
+					
+					}
+					listalibros.add(book);
 				}
-				aLb.add(lb);
-				// System.out.println("Nombre =" + detalle.getName());
-				// System.out.println("Valor =" + detalle.getValue());
-
+				//Muestra mi array de libros que está en memoria
+//				for (Libro l : listalibros) {
+//				
+//					System.out.println(l);
+//				}
+				
+				//3. Ordeno ArrayList(por título)
+				
+				Collections.sort(listalibros);// Ordena mi array de libros por el nombre	
+				
+				Element librosxml = new Element("libros");
+			
+				for (Libro li : listalibros) {
+					
+					librosxml.addContent(LibroAElemento(li));
+					
+				}
+				Document doc = new Document(librosxml);
+				
+				Format format = Format.getPrettyFormat();
+			    // Creamos el serializador con el formato deseado  
+			    XMLOutputter xmloutputter = new XMLOutputter(format);
+			    // Serializamos el document parseado  
+			    String docStr = xmloutputter.outputString(doc); 
+			    //Volcamos en un fichero
+			    FileWriter fw = new FileWriter("practicaJDOM.xml");
+			    fw.write(docStr);
+			    fw.close();
 			}
-
-			Collections.sort(aLb);
-		}
-		// for (Libro lbs : aLb) {
-		// System.out.println(lbs);
-		// } muestra mi array list.
-
-		// AÑADO UN NUEVO HIJO
-		Element padre = documentJDOM.getRootElement();// libros
-		// Creamos una nueva etiqueta
-		Element nuevolibro = new Element("libro");
-		// Añadimos un atributo
-		nuevolibro.setAttribute("isbn", "xxxx-yyyy");
-
-		Element titulo = new Element("titulo");
-		titulo.addContent("Dragonlance");// aqui indicamos si es dentro de padre
-											// o
-											// dentro de isbn.
-		nuevolibro.addContent(titulo);// estas añadiendo la etiqueta titulo al
-										// libro
-		padre.addContent(nuevolibro);// Al padre le he añadido el libro
-
-		// SERIALIZO EL DOCUMENT A UN FICHERO DE SALIDA
-
-		Format format = Format.getPrettyFormat();
-		// Creamos el serializador con el formato deseado
-		XMLOutputter xmloutputter = new XMLOutputter(format);
-		// Serializamos el document parseado
-		String docStr = xmloutputter.outputString(documentJDOM);
-		// Volcamos en un fichero
-		FileWriter fw = new FileWriter("lsalida3.xml");
-		fw.write(docStr);
-		fw.close();
-
+			
+			public static Element LibroAElemento (Libro l) {
+				
+				Element elibro = new Element("libro");
+				Element etitulo = new Element("titulo");
+				Element eautor = new Element("autor");
+				Element eanyo = new Element("anyo");
+				Element eeditorial = new Element("editorial");
+				
+				etitulo.addContent(l.getTitulo());
+				eautor.addContent(l.getAnyo());
+				eanyo.addContent(l.getAnyo());
+				eeditorial.addContent(l.getEditorial());
+				
+				elibro.setAttribute("isbn", l.getIsbn());
+				elibro.addContent(etitulo);
+				elibro.addContent(eautor);
+				elibro.addContent(eanyo);
+				elibro.addContent(eeditorial);
+				
+				return elibro;
 	}
 }
